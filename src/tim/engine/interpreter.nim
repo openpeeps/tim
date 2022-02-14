50a36a99346843390b5feedd734cec7fcd7daf53
George Lemon <georgelemon@protonmail.com>
2022-02-14 20:48:34 +0200

    Interpreter work
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/tim/engine/interpreter.nim
+++ src/tim/engine/interpreter.nim
@@ -6,10 +6,49 @@
 
 import std/tables
 import std/json
+import std/macros
 
 type 
     Interpreter* = object
         data: JsonNode
+
+macro `?`*(a: bool, body: untyped): untyped =
+    let b = body[1]
+    let c = body[2]
+    result = quote:
+        if `a`: `b` else: `c`
+
+macro isEqualBool*(a, b: bool): untyped =
+    result = quote:
+        `a` == `b`
+
+macro isNotEqualBool*(a, b: bool): untyped =
+    result = quote:
+        `a` != `b`
+
+macro isEqualInt*(a, b: int): untyped =
+    result = quote:
+        `a` == `b`
+
+macro isNotEqualInt*(a, b: int): untyped =
+    result = quote:
+        `a` != `b`
+
+macro isEqualFloat*(a, b: float64): untyped =
+    result = quote:
+        `a` == `b`
+
+macro isNotEqualFloat*(a, b: float64): untyped =
+    result = quote:
+        `a` != `b`
+
+macro `isEqualString`*(a, b: string): untyped =
+    result = quote:
+        `a` == `b`
+
+macro `isNotEqualString`*(a, b: string): untyped =
+    result = quote:
+        `a` != `b`
 
 proc hasVar*[T: Interpreter](i: T, key: string): bool =
     ## Determine if current data contains given variable key
