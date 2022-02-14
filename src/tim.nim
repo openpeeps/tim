69936ab620aa44ad0c5b4bc06404dd47f6b735b0
George Lemon <georgelemon@protonmail.com>
2022-02-14 20:48:55 +0200

    mainModule
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/tim.nim
+++ src/tim.nim
@@ -1,19 +1,50 @@
-# ⚡️ High-performance compiled
-# template engine inspired by Emmet syntax.
+# High-performance, compiled template engine inspired by Emmet syntax.
 # 
 # MIT License
 # Copyright (c) 2022 George Lemon from OpenPeep
 # https://github.com/openpeep/tim
 
-import tim/[parser, compiler]
+import std/[tables, json]
+import tim/engine/[parser, compiler]
+
 from std/times import cpuTime
 from std/os import getCurrentDir
 
 export parser, compiler
 
+type 
+    TimlPageType = enum
+        Layout, View, Partial
+
+    TimlPage = object
+        id, filepath: string
+        timlType: TimlPageType
+
+    TimEngine* = object
+        pages: OrderedTable[string, TimlPage]
+
+proc init*[T: typedesc[TimEngine]](engine: T): TimEngine =
+    ## Initialize Tim Engine
+    var e = engine()
+
+# proc addLayout*[T: TimEngine](engine: T, page: TimlPage) =
+#     ## Add a new Timl layout to current Engine instance
+
+proc add*[T: TimEngine](pageType: TimlPageType) =
+    ## Add a new Timl layout, view or partial to current Engine instance
+    discard
+
 when isMainModule:
     let time = cpuTime()
-    var p: Parser = parse(readFile("sample.timl"))
+
+    # Create a new Tim Engine | For development purpose
+    # var engine = TimEngine.init()
+
+    # Add a new Timl layout, view, or partial using `add` proc
+    # engine.add(pageType = Layout)
+    let sampleData = readFile(getCurrentDir() & "/sample.json")
+    var p: Parser = parse(readFile("sample.timl"), data = parseJson(sampleData))
+
     if p.hasError():
         # Catch errors collected while parsing
         echo p.getError()
@@ -26,5 +57,6 @@ when isMainModule:
         let c = Compiler.init(parser = p, minified = false)
         echo "✨ Done in " & $(cpuTime() - time)
 
-        let sample = getCurrentDir() & "/sample.html"
-        writeFile(sample, c.getHtml())
+        # let sample = getCurrentDir() & "/sample.html"
+        # writeFile(sample, c.getHtml())
+        echo c.getHtml()
