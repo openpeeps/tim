d2d75266bc466c515589d4605f091542b3829cb3
George Lemon <georgelemon@protonmail.com>
2022-02-16 12:53:35 +0200

    WIP TimEngine
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/tim.nim
+++ src/tim.nim
@@ -4,59 +4,50 @@
 # Copyright (c) 2022 George Lemon from OpenPeep
 # https://github.com/openpeep/tim
 
-import std/[tables, json]
-import tim/engine/[parser, compiler]
+import tim/engine/[parser, compiler, meta]
+import std/tables
 
 from std/times import cpuTime
-from std/os import getCurrentDir
+from std/strutils import `%`, strip, split
+from std/osproc import execProcess, poStdErrToStdOut, poUsePath
+from std/os import getCurrentDir, normalizePath, dirExists,
+                   fileExists, walkDirRec, splitPath
 
-export parser, compiler
+export parser, compiler, meta
 
-type 
-    TimlPageType = enum
-        Layout, View, Partial
-
-    TimlPage = object
-        id, filepath: string
-        timlType: TimlPageType
-
-    TimEngine* = object
-        pages: OrderedTable[string, TimlPage]
-
-proc init*[T: typedesc[TimEngine]](engine: T): TimEngine =
-    ## Initialize Tim Engine
-    var e = engine()
-
-# proc addLayout*[T: TimEngine](engine: T, page: TimlPage) =
-#     ## Add a new Timl layout to current Engine instance
-
-proc add*[T: TimEngine](pageType: TimlPageType) =
-    ## Add a new Timl layout, view or partial to current Engine instance
+proc run*[T: TimEngine](engine: var T) =
+    echo len(engine.getSources())
     discard
 
 when isMainModule:
     let time = cpuTime()
 
-    # Create a new Tim Engine | For development purpose
-    # var engine = TimEngine.init()
+    # First, create a new Tim Engine
+    var engine = TimEngine.init(
+        templates = "../examples/templates",          # root dir to Timl templates
+        storage = "../examples/storage/templates"     # dir path for storing BSON AST files 
+        hotreload = true                              # automatically disabled when compiling with -d:release
+    )
+    engine.run()
+    echo "✨ Done in " & $(cpuTime() - time)
 
     # Add a new Timl layout, view, or partial using `add` proc
     # engine.add(pageType = Layout)
-    let sampleData = readFile(getCurrentDir() & "/sample.json")
-    var p: Parser = parse(readFile("sample.timl"), data = parseJson(sampleData))
+    # let sampleData = readFile(getCurrentDir() & "/sample.json")
+    # var p: Parser = parse(readFile("sample.timl"), data = parseJson(sampleData))
 
-    if p.hasError():
-        # Catch errors collected while parsing
-        echo p.getError()
-    else:
-        # Returns the a stringified JSON representing the
-        # Abstract Syntax Tree of the current timl document
-        echo p.getStatements()
+    # if p.hasError():
+    #     # Catch errors collected while parsing
+    #     echo p.getError()
+    # else:
+    #     # Returns the a stringified JSON representing the
+    #     # Abstract Syntax Tree of the current timl document
+    #     var BSONDocument = @@(p.getStatements())
+    #     writeFile(getCurrentDir() & "/sample.ast.bson", BSONDocument.bytes())
 
-        # Otherwise compile timl document to html
-        let c = Compiler.init(parser = p, minified = false)
-        echo "✨ Done in " & $(cpuTime() - time)
+    #     # Otherwise compile timl document to html
+    #     let c = Compiler.init(parser = p, minified = false)
+    #     echo "✨ Done in " & $(cpuTime() - time)
 
-        # let sample = getCurrentDir() & "/sample.html"
-        # writeFile(sample, c.getHtml())
-        echo c.getHtml()
+    #     writeFile(getCurrentDir() & "/sample.html", c.getHtml())
+    #     echo c.getHtml()
