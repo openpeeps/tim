1d6aede375e351fc21944cc35832bf857367537e
George Lemon <georgelemon@protonmail.com>
2022-02-19 20:51:43 +0200

    Tim main
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/tim.nim
+++ src/tim.nim
@@ -4,20 +4,53 @@
 # Copyright (c) 2022 George Lemon from OpenPeep
 # https://github.com/openpeep/tim
 
-import tim/engine/[parser, compiler, meta]
-import std/[tables, json]
-from std/times import cpuTime
-export parser, compiler, meta
+import tim/engine/[parser, compiler, jit, meta]
+import std/[tables, json, strutils]
 
-proc compile*[T: TimEngine](engine: var T) =
+import bson
+
+from tim/engine/ast import HtmlNode
+from std/times import cpuTime
+
+export parser, compiler, meta, jit
+
+proc compile*[T: TimEngine](engine: var T, debug = false) =
+    ## Parse Tim's layouts, views and partials and compile
+    ## to BSON Abstract Syntax Tree
+
+    var data = %*{
+        "firstname": "George",
+        "lastname": "Lemon"
+    }
+
     if engine.hasAnySources:
         for id, layout in engine.getLayouts().pairs():
-            var p: Parser = engine.parse(layout, data = %*{})
+            
+            # Parse each template.
+            var p: Parser = engine.parse(layout, data = data)
             if p.hasError():
-                echo p.getError()
+                raise newException(TimSyntaxError, "\n"&p.getError())
+            if debug:
+                # AST Nodes to BSON AST
+                # BSON files are saved to provided `output` path under `bson` directory.
+                # Each BSON template is named using MD5 based on its absolute path
+                if p.hasJIT:
+                    engine.writeBson(layout, p.getStatementsStr())
+                    # RUNTIME!
+                    # Now, this is supposed to be done on runtime (on request).
+                    # Here we get the BSON AST and, if needed it will be sent 
+                    # to the evaluator to determine what should be displayed or not
+                    # TODO
+                    let c = JIT.init(engine.readBson(layout), minified = false)
+                    echo c.getHtml
+                else:
+                    echo "compile to html"
+                    # Finally, compile the AST to HTML
+                    let c = Compiler.init(parser = p, minified = false, asNodes = true)
+                    # echo c.getHtml
             else:
-                # layout.setAstSource(p.getStatements())        # fix 'layout' is immutable, not 'var'
-                engine.writeBson(layout, p.getStatements())
+                # Save the Abstract Syntax Tree of the current template as BSON
+                engine.writeBson(layout, p.getStatementsStr())
     else: raise newException(TimException, "Unable to find any Timl templates")
 
 when isMainModule:
@@ -28,24 +61,5 @@ when isMainModule:
         output = "../examples/storage/templates",     # dir path for storing BSON AST files 
         hotreload = true                              # automatically disabled when compiling with -d:release
     )
-    engine.compile()
+    engine.compile(debug = true)
     echo "✨ Done in " & $(cpuTime() - time)
-
-    # Add a new Timl layout, view, or partial using `add` proc
-    # engine.add(pageType = Layout)
-    # let sampleData = readFile(getCurrentDir() & "/sample.json")
-    # var p: Parser = parse(readFile("sample.timl"), data = parseJson(sampleData))
-
-    # if p.hasError():
-    #     # Catch errors collected while parsing
-    #     echo p.getError()
-    # else:
-    #     # Returns the a stringified JSON representing the
-    #     # Abstract Syntax Tree of the current timl document
-
-    #     # Otherwise compile timl document to html
-    #     let c = Compiler.init(parser = p, minified = false)
-    #     echo "✨ Done in " & $(cpuTime() - time)
-
-    #     writeFile(getCurrentDir() & "/sample.html", c.getHtml())
-    #     echo c.getHtml()
