0f632a7ce665d6f5ffa0e2bc35e3fb53b0cc92f0
George Lemon <georgelemon@protonmail.com>
2022-12-18 19:21:22 +0200

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -8,23 +8,20 @@ Tim.init(
     indent = 4
 )
 
-# Tim.setData(%*{
-#     "appName": "My application",
-#     "production": false,
-#     "keywords": ["template-engine", "html", "tim", "compiled", "templating"]
-# })
+Tim.setData(%*{
+    "appName": "My application",
+    "production": false,
+    "keywords": ["template-engine", "html", "tim", "compiled", "templating"]
+})
 
 test "can init":
-    assert dirExists("./examples/templates") == true
-    assert dirExists("./examples/templates") == true
-    assert fileExists("./examples/templates/layouts/base.timl") == true
-    # assert Tim.hasAnySources == true
-#     assert Tim.getIndent == 4
-#     assert Tim.shouldMinify == false
+    assert Tim.hasAnySources == true
+    assert Tim.getIndent == 4
+    assert Tim.shouldMinify == false
 
-# test "can precompile":
-#     let timlFiles = Tim.precompile()
-#     assert timlFiles.len != 0
+test "can precompile":
+    let timlFiles = Tim.precompile()
+    assert timlFiles.len != 0
 
-# test "can render":
-#     echo Tim.render("index")
+test "can render":
+    echo Tim.render("index")
