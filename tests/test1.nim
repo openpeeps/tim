9c3a7d753448e270e18bb5c366324b7df746198a
George Lemon <georgelemon@protonmail.com>
2022-12-18 17:08:11 +0200

    update test
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,16 +1,27 @@
-import unittest, tim
+import std/[unittest, json], tim
 from std/os import getCurrentDir
 
 Tim.init(
-    source = "../examples/templates",
-    output = "../examples/storage/templates",
+    source = getCurrentDir() & "/examples/templates",
+    output = getCurrentDir() & "/examples/storage/templates",
     minified = false,
     indent = 4
 )
 
+Tim.setData(%*{
+    "appName": "My application",
+    "production": false,
+    "keywords": ["template-engine", "html", "tim", "compiled", "templating"]
+})
+
 test "can init":
-    echo "todo"
+    assert Tim.hasAnySources == true
+    assert Tim.getIndent == 4
+    assert Tim.shouldMinify == false
 
+test "can precompile":
+    let timlFiles = Tim.precompile()
+    assert timlFiles.len != 0
 
-test "can prevent duplicate attrs":
-    echo "todo"
+test "can render":
+    echo Tim.render("index")
