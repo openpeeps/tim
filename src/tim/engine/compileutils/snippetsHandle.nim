53385af97d4aa39fcfb50312076a8e05b6513218
George Lemon <georgelemon@protonmail.com>
2023-02-04 16:07:48 +0200

    cleanup
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/tim/engine/compileutils/snippetsHandle.nim
+++
@@ -1,8 +0,0 @@
-proc handleJavaScriptSnippet(c: var Compiler, node: Node) =
-  c.js &= node.jsCode
-
-proc handleSassSnippet(c: var Compiler, node: Node) =
-  try:
-    c.sass &= NewLine & compileSass(node.sassCode)
-  except SassException:
-    c.logs.add(getCurrentExceptionMsg())
