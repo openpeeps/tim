2491bf97ee451af3f782a878cd9d0484d0503919
George Lemon <georgelemon@protonmail.com>
2023-06-04 20:37:25 +0300

    add tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,76 +1,82 @@
-import std/[unittest, json], tim
+import tim
+import std/[unittest, json, htmlparser, xmltree, strtabs]
 from std/os import getCurrentDir, dirExists, fileExists
 
 Tim.init(
-    source = "./examples/templates",
-    output = "./examples/storage/templates",
-    minified = false,
-    indent = 2
+  source = "./examples/templates",
+  output = "./examples/storage/templates",
+  minified = false,
+  indent = 2
 )
 
 Tim.setData(%*{
-    "appName": "My application",
-    "production": false,
-    "keywords": ["template-engine", "html", "tim", "compiled", "templating"],
-    "products": [
-        {
-            "id": "8629976",
-            "name": "Riverside 900, 10-Speed Hybrid Bike",
-            "price": 799.00,
-            "currency": "USD",
-            "composition": "100% Aluminium 6061",
-            "sizes": [
-                {
-                    "label": "S",
-                    "stock": 31
-                },
-                {
-                    "label": "M",
-                    "stock": 94
-                },
-                {
-                    "label": "L",
-                    "stock": 86
-                }
-            ]
-        },
-        {
-            "id": "8219006",
-            "name": "Riverside 500, 7-Speed Hybrid Bike",
-            "price": 599.00,
-            "currency": "USD",
-            "composition": "100% Aluminium 6061",
-            "sizes": [
-                {
-                    "label": "S",
-                    "stock": 31
-                },
-                {
-                    "label": "M",
-                    "stock": 94
-                },
-                {
-                    "label": "L",
-                    "stock": 86
-                }
-            ]
-        }
-    ],
-    "objects": {
-        "a" : {
-            "b": "ok"
-        }
-    }
+  "appName": "My application",
+  "production": false,
+  "keywords": ["template-engine", "html", "tim", "compiled", "templating"],
 })
 
 test "can init":
-    assert Tim.hasAnySources == true
-    assert Tim.getIndent == 2
-    assert Tim.shouldMinify == false
+  check Tim.templatesExists == true
+  check Tim.getIndent == 2
+  check Tim.shouldMinify == false
 
 test "can precompile":
-    let timlFiles = Tim.precompile()
-    assert timlFiles.len != 0
+  Tim.precompile()
 
-test "can render":
-    echo Tim.render("index")
+test "can render (file)":
+  let output = Tim.render("index").parseHtml
+  for h1 in output.findAll("h1"):
+    check h1.attrs.hasKey("class") == true
+    check h1.attrs["class"] == "fw-bold"
+    check h1.attrsLen == 1
+    check h1.innerText == "This is Tim Engine!"
+  for p in output.findAll("p"):
+    check p.attrs.hasKey("class") == true
+    check p.attrs["class"] == "lead"
+    check p.attrsLen == 1
+    check p.innerText == "A high-performance template engine & markup language"
+
+test "can render (code)":
+  var output = tim2html("div > span: \"Hello\"", true)
+  check output == "<div><span>Hello</span></div>"
+
+  output = tim2html("""
+a.text-link href="https://openpeeps.github.io/tim/": "API Reference"
+  """)
+  let xmlcode = output.parseHtml
+  for a in xmlcode.findAll("a"):
+    check a.attrsLen == 2
+    check a.attrs["class"] == "text-link"
+
+test "can render loop (code)":
+  var output = tim2html("""
+for $x in $this.list:
+  span: $x
+  """, data = %*{
+    "list": ["one", "two", "three"]
+  })
+  check output.parseHtml.findAll("span").len == 3
+
+test "can render conditionals (code)":
+  var output = tim2html("""
+if $this.hello == "world"
+  span: "Hello World"
+
+if $this.enabled != true:
+  span: "Disabled"
+else:
+  span: "Enabled"
+
+if $this.counter < 120:
+  span: "less than 120"
+elif $this.counter >= 120:
+  span: "greater or equal"
+else:
+  span > u: "nothing here"
+
+  """, data = %*{
+    "hello": "world",
+    "enabled": true,
+    "counter": 120
+  })
+  echo output
