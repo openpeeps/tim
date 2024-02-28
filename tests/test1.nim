9d2a0245d333c051a718f9a928de22e6a9604ca9
George Lemon <georgelemon@protonmail.com>
2024-02-28 04:53:27 +0200

    wip tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,4 +1,4 @@
-import std/unittest
+import std/[unittest, htmlparser, xmltree, strtabs, sequtils]
 import ../src/tim
 
 var t = newTim("./app/templates", "./app/storage",
@@ -9,3 +9,18 @@ test "precompile":
 
 test "render index":
   echo t.render("index")
+
+test "check layout":
+  let html = t.render("index").parseHtml
+  # check `meta` tags
+  let meta = html.findAll("meta").toSeq
+  check meta.len == 2
+  check meta[0].attrs["charset"] == "utf-8"
+
+  check meta[1].attrsLen == 2
+  check meta[1].attrs.hasKey("name")
+  check meta[1].attrs.hasKey("content")
+
+  let title = html.findAll("title").toSeq
+  check title.len == 1
+  check title[0].innerText == "Tim Engine is Awesome!"
