d42bc546a38e941663bedeed687b2d2744d33471
George Lemon <georgelemon@protonmail.com>
2024-05-20 15:28:17 +0300

    add more tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -21,13 +21,12 @@ test "check layout":
   check meta[1].attrs.hasKey("name")
   check meta[1].attrs.hasKey("content")
 
-
   let title = html.findAll("title").toSeq
   check title.len == 1
   check title[0].innerText == "Tim Engine is Awesome!"
 
 import std/sequtils
-import ../src/tim/engine/[logging, parser, compilers/html]
+import ../src/timpkg/engine/[logging, parser, compilers/html]
 
 proc toHtml(id, code: string): (Parser, HtmlCompiler) =
   result[0] = parseSnippet(id, code)  
@@ -44,7 +43,7 @@ var b = {}
   check x[1].hasErrors == false
 
 test "language test const":
-  const code = """
+  let code = """
 const x = 123
 h1: $x
 $x = 321
@@ -52,4 +51,63 @@ $x = 321
   let x = toHtml("test_var", code)
   check x[0].hasErrors == false
   check x[1].hasErrors == true
-  # echo x[1].logger.errors.toSeq()
+
+test "conditions if":
+  let code = """
+if 0 == 0:
+  span: "looks true to me""""
+  assert tim.toHtml("test_if", code) ==
+    """<span>looks true to me</span>"""
+
+test "conditions if/else":
+  let code = """
+if 1 != 1:
+  span: "looks true to me"
+else:
+  span.just-some-basic-stuff: "this is basic""""
+  assert tim.toHtml("test_if", code) ==
+    """<span class="just-some-basic-stuff">this is basic</span>"""
+
+test "conditions if/elif":
+  let code = """
+if 1 != 1:
+  span: "looks true to me"
+elif 1 == 1:
+  span.just-some-basic-stuff: "this is basic""""
+  assert tim.toHtml("test_if", code) ==
+    """<span class="just-some-basic-stuff">this is basic</span>"""
+
+test "conditions if/elif/else":
+  let code = """
+if 1 != 1:
+  span: "looks true to me"
+elif 1 > 1:
+  span.just-some-basic-stuff: "this is basic"
+else:
+  span"""
+  assert tim.toHtml("test_if", code) ==
+    """<span></span>"""
+
+test "loops for":
+  let code = """
+var fruits = ["satsuma", "watermelon", "orange"]
+for $fruit in $fruits:
+  span data-fruit=$fruit: $fruit
+  """
+  assert tim.toHtml("test_loops", code) ==
+    """<span data-fruit="satsuma">satsuma</span><span data-fruit="watermelon">watermelon</span><span data-fruit="orange">orange</span>"""
+
+test "loops using * multiplier":
+  let code = """
+const items = ["keyboard", "speakers", "mug"]
+li * 3: $i + 1 & " - " & $items[$i]"""
+  assert tim.toHtml("test_multiplier", code) ==
+    """<li>1 - keyboard</li><li>2 - speakers</li><li>3 - mug</li>"""
+
+test "loops using * var multiplier":
+  let code = """
+const x = 3
+const items = ["keyboard", "speakers", "mug"]
+li * $x: $items[$i]"""
+  assert tim.toHtml("test_multiplier", code) ==
+    """<li>keyboard</li><li>speakers</li><li>mug</li>"""
