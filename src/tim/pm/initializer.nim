a4cb41b1f078e91a4d1a137b869fb99265e7788b
George Lemon <georgelemon@protonmail.com>
2026-02-06 04:29:46 +0200

    add dummy ws
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ src/tim/pm/initializer.nim
@@ -0,0 +1,366 @@
+import std/[os, tables, net, strutils, json, sequtils, options]
+
+# import pkg/voodoo/language/pm/manager
+import pkg/voodoo/language/[ast, codegen, chunk, sym, vm, value]
+import pkg/voodoo/packagemanager/[configurator, packager]
+
+import pkg/[watchout, checksums/sha1]
+import ../engine/parser
+
+export value
+
+when defined timHotCode:
+  import ./websocket
+
+#
+# Standard Libraries
+# 
+import ../engine/stdlib/[libsystem, libffi,  libtimes, libstrings, inliner]
+
+
+export TypeKind, StackView, Value, CodeGenError
+export configurator, paramDef
+
+type
+  TimTemplateType* = enum
+    ## Type of the Tim template
+    ttView = "views"
+    ttLayout = "layouts"
+    ttPartial = "partials"
+
+  TimTemplate* = ref object
+    ## Object representing a Tim template
+    id*: string
+      ## unique identifier of the template (based on path hash)
+    src*: string
+      ## the absolute path to the template source file
+    templateType*: TimTemplateType
+      ## the type of the template (view, layout or partial)
+    script: Script
+      ## the transpiled script of the template
+    mainChunk: Chunk
+      ## the main chunk of the transpiled script
+
+  UserScript* = ref object
+    # chunk: Chunk
+    # script: Script
+    # module: Module
+    procs: seq[(string, seq[TempParamDef], TypeKind, ForeignProc, bool)]
+      ## A sequence of foreign procedures to be added to the user script
+    code: string # A string to inject into the script before execution
+
+  TimEngine* = ref object
+    ## The main Tim Engine object.
+    ## Holds the configuration and the templates.
+    ## 
+    ## It must be initialized with `newTim()`
+    config*: TimConfig
+    userScript*: UserScript
+    globalData*: JsonNode
+      ## Global data available in all templates under the `$app` variable
+    views*, layouts*, partials*: TableRef[string, TimTemplate] = newTable[string, TimTemplate]()
+      ## Tables to store templates by their source path
+  
+  TimEngineError* = object of CatchableError
+
+let stdlibs = newTable[string, proc(script: Script, systemModule: Module): Module]()
+
+iterator getViews*(engine: TimEngine): TimTemplate =
+  ## Iterator to get all view templates
+  for _, tpl in engine.views:
+    yield tpl
+
+iterator getLayouts*(engine: TimEngine): TimTemplate =
+  ## Iterator to get all layout templates
+  for _, tpl in engine.layouts:
+    yield tpl
+
+iterator getPartials*(engine: TimEngine): TimTemplate =
+  ## Iterator to get all partial templates
+  for _, tpl in engine.partials:
+    yield tpl
+
+proc precompile*(engine: TimEngine, firstRun: bool = false) # forward declaration
+
+proc transpile*(engine: TimEngine, tpl: TimTemplate,
+        pkgr: Packager, data: JsonNode = nil): bool {.discardable.}
+
+proc getHashedPath(path: string): string =
+  ## Get a SHA1 hash of the given path.
+  $(sha1.secureHash(path))
+
+proc newTemplate*(id: string, templateType: TimTemplateType, src: string): TimTemplate =
+  ## Create a new TimTemplate object.
+  result = TimTemplate(
+    id: id,
+    src: src,
+    templateType: templateType
+  )
+
+proc addProc*(userScript: UserScript, name: string, params: seq[TempParamDef] = @[],
+        returnTy: TypeKind, impl: ForeignProc = nil, exportSym = true) =
+  ## Add a foregin function to the `UserScript`.
+  userScript.procs.add((name, params, returnTy, impl, exportSym))
+
+proc injectScript*(userScript: UserScript, code: string) =
+  ## Inject a code string into the `UserScript`.
+  # userScript.script.compileCode(userScript.module, "user_script", code)
+
+proc newTim*(src, output, basepath: string,
+          target = TargetSource.tsHtml,
+          globalData: JsonNode = newJObject()
+  ): TimEngine =
+  ## Initialize a new Tim Engine instance.
+  ## 
+  ## `src`: the source directory containing the templates
+  ## 
+  ## `output`: the output directory where the rendered files will be saved
+  ## 
+  ## `basepath`: the base path to resolve the `src` and `output` paths
+  ## 
+  ## `target`: the target source for transpilation (default: HTML)
+  let sourcePath = normalizedPath(basepath / src)
+  result = TimEngine(
+    userScript: UserScript(),
+    globalData: globalData,
+    config: TimConfig(
+      `type`: ConfigType.typeProject,
+      compilation: CompilationSettings(
+        source: sourcePath,
+        output: output,
+        basePath: basepath,
+        layoutsPath: sourcePath / "layouts",
+        viewsPath: sourcePath / "views",
+        partialsPath: sourcePath / "partials",
+        target: target
+      )
+    )
+  )
+
+  stdlibs["times"] = loadTimes
+  stdlibs["ffi"] = loadFFI
+
+proc getTemplateByPath*(engine: TimEngine, path: string): TimTemplate =
+  ## Get a Tim template by its source path.
+  ## Returns a TimTemplate object with the template type set to `ttView`.
+  if path in engine.views:
+    return engine.views[path]
+
+  if path in engine.layouts:
+    return engine.layouts[path]
+
+  if path in engine.partials:
+    return engine.partials[path]
+
+proc getLayout*(engine: TimEngine, key: string): TimTemplate =
+  ## Get a layout template by its name (with/without extension).
+  let path = engine.config.compilation.layoutsPath / key
+  if not key.endsWith(".timl"):
+    return engine.layouts.getOrDefault(path & ".timl", nil)
+  return engine.layouts.getOrDefault(path, nil)
+
+proc getView*(engine: TimEngine, key: string): TimTemplate =
+  ## Get a view template by its name (with/without extension).
+  let path = engine.config.compilation.viewsPath / key
+  if not key.endsWith(".timl"):
+    return engine.views.getOrDefault(path & ".timl", nil)
+  return engine.views.getOrDefault(path, nil)
+
+proc getPartial*(engine: TimEngine, key: string): TimTemplate =
+  ## Get a partial template by its name (with/without extension).
+  let path = engine.config.compilation.partialsPath / key
+  if not key.endsWith(".timl"):
+    return engine.partials.getOrDefault(path & ".timl", nil)
+  return engine.partials.getOrDefault(path, nil)
+
+proc registerTemplate*(engine: TimEngine, src: string): TimTemplate =
+  ## Register a new Tim template. Returns a `TimTemplate` object
+  var templateType: TimTemplateType
+  if src.startsWith(engine.config.compilation.viewsPath):
+    templateType = ttView
+  elif src.startsWith(engine.config.compilation.layoutsPath):
+    templateType = ttLayout
+  elif src.startsWith(engine.config.compilation.partialsPath):
+    templateType = ttPartial
+  let tpl = TimTemplate(src: src, templateType: templateType)
+  case templateType
+  of ttView:
+    engine.views[src] = tpl
+  of ttLayout:
+    engine.layouts[src] = tpl
+  of ttPartial:
+    engine.partials[src] = tpl
+  return tpl
+
+proc parserCallback(astProgram: var Ast, path: string) =
+  parser.parseScript(astProgram, readFile(path), path)
+
+proc transpile*(engine: TimEngine, tpl: TimTemplate,
+        pkgr: Packager, data: JsonNode = nil): bool {.discardable.} =
+  ## Transpile the Tim template code.
+  var astProgram: Ast
+  try:
+    parser.parseScript(astProgram, readFile(tpl.src), tpl.src)
+  except TimParserError as e:
+    echo "Error parsing template: ", e.msg
+    echo tpl.src
+    return
+
+  var
+    mainChunk = newChunk(tpl.src)
+    script = newScript(mainChunk)
+    module = newModule(tpl.src.extractFilename, some(tpl.src))
+    localData = newJObject()
+
+  # load standard library modules
+  let systemModule = libsystem.loadLibrary(script, engine.globalData, localData)
+  module.load(systemModule)
+
+  # load the user defined script
+  if engine.userScript != nil:
+    for procDef in engine.userScript.procs:
+      script.addProc(
+        module,
+        procDef[0], # name
+        procDef[1], # params
+        procDef[2], # return type
+        procDef[3], # implementation
+        procDef[4]  # export symbol
+      )
+    # module.load(engine.userScript.module)
+
+  let stringsLib = initStrings(script, systemModule)
+  module.load(stringsLib)
+
+  # module.load(stdlibs["ffi"](script, systemModule))
+  # module.load(stdlibs["times"](script, systemModule))
+
+  script.stdpos = script.procs.high
+  
+  var compiler =
+    codegen.initCompiler(script, module, mainChunk,
+                    pkgr, stdlibs, parserCallback)
+  compiler.genScript(
+    program = astProgram,
+    includePath = some(engine.config.compilation.partialsPath)
+  )
+  tpl.script = script
+  tpl.mainChunk = mainChunk
+  return true
+
+var
+  browserSyncWatcher: Watchout
+  hasChanges: bool
+
+proc eval*(view, layout: TimTemplate,
+        localData, globalData: JsonNode): string {.raises: [IndexDefect, ValueError, KeyError, TimEngineError, Exception].} =
+  ## Evaluate a view within a layout and return the final HTML output.
+  assert view.script != nil and layout.script != nil,
+    "View or Layout script is not initialized"
+  let
+    viewVM = newVM()
+    layoutVM = newVM()
+  let viewOutput = viewVM.interpret(view.script, view.mainChunk, localData = localData)
+  return layoutVM.interpret(layout.script, layout.mainChunk, some(viewOutput), localData = localData)
+
+proc precompile*(engine: TimEngine, firstRun: bool = false) =
+  ## Precompile Tim Engine templates.
+  ## 
+  ## This proc is usually called before starting the development server.
+  ## 
+  ## When the browser sync is enabled, it will watch for changes in the templates
+  ## and recompile them on the fly.
+
+  # init the package manager and load the local packages
+  let pkgr = packager.initPackageRemote()
+  pkgr.loadPackages()
+
+  for sourceDir in [ttLayout, ttView, ttPartial]:
+    if not dirExists(engine.config.compilation.source / $sourceDir):
+      raise newException(TimEngineError, "Missing directory $1: \n$2" % [$sourceDir, engine.config.compilation.source / $sourceDir])
+    for srcPath in walkDirRec(engine.config.compilation.source / $sourceDir):
+      let
+        id = getHashedPath(srcPath) # unique id based on path
+        astPath = engine.config.compilation.output / "ast" / id & ".ast"
+        htmlPath = engine.config.compilation.output / "html" / id & ".html"
+        sources = (src: srcPath, ast: astPath, html: htmlPath)
+      case sourceDir:
+        of ttLayout:
+          let tpl = newTemplate(id, ttLayout, sources.src)
+          if engine.transpile(tpl, pkgr):
+            engine.layouts[srcPath] =  tpl
+        of ttView:
+          let tpl = newTemplate(id, ttView, sources.src)
+          if engine.transpile(tpl, pkgr):
+            engine.views[srcPath] = tpl 
+        of ttPartial:
+          let tpl = newTemplate(id, ttPartial, sources.src)
+          if engine.transpile(tpl, pkgr):
+            engine.partials[srcPath] = tpl
+        else: discard
+
+  browserSyncWatcher = 
+    newWatchout(@[
+      engine.config.compilation.layoutsPath,
+      engine.config.compilation.viewsPath,
+      engine.config.compilation.partialsPath
+    ], some("*.timl"))
+
+  # Callback `onFound`
+  proc onFound(file: watchout.File) =
+    # Runs when detecting a new template.
+    let tpl: TimTemplate = engine.getTemplateByPath(file.getPath())
+    if tpl != nil:
+      case tpl.templateType
+      of ttView, ttLayout:
+        engine.transpile(tpl, pkgr)
+      else: discard
+    else:
+      # if the template is not registered,
+      # we need to register it and compile it
+      let newTpl = engine.registerTemplate(file.getPath())
+      if newTpl.templateType != ttPartial:
+        # partials don't need to be compiled as they
+        # are included in other templates (layouts or views)
+        engine.transpile(newTpl, pkgr)
+
+  # Callback `onChange`
+  proc onChange(file: watchout.File) =
+    # Runs when detecting changes
+    let tpl: TimTemplate = engine.getTemplateByPath(file.getPath())
+    if tpl == nil: return # template not found, ignore
+    case tpl.templateType
+    of ttView, ttLayout:
+      # if the template is a view or layout, compile it
+      engine.transpile(tpl, pkgr)
+      when defined timHotCode:
+        notifyAllClients()
+    else:
+      # otherwise, we'll need to search for the templates
+      # that load the current partial template and recompile them
+      discard
+      # engine.importsHandle.excl(file.getPath())
+      # engine.resolveDependants(engine.importsHandle.dependencies(file.getPath).toSeq)
+    hasChanges = true
+
+  # Callback `onDelete`
+  proc onDelete(file: watchout.File) =
+    # Runs when detecting a deleted template
+    let tpl: TimTemplate = engine.getTemplateByPath(file.getPath())
+    if tpl != nil:
+      case tpl.templateType
+      of ttView:
+        engine.views.del(tpl.src)
+      of ttLayout:
+        engine.layouts.del(tpl.src)
+      of ttPartial:
+        engine.partials.del(tpl.src)
+
+  when defined timHotCode:
+    startWebSocket(port = Port(9000))
+    sleep(100)
+
+  browserSyncWatcher.onFound = onFound
+  browserSyncWatcher.onChange = onChange
+  browserSyncWatcher.onDelete = onDelete
+  browserSyncWatcher.start()
---
+++ src/tim/pm/websocket.nim
@@ -0,0 +1,145 @@
+# A super fast template engine for cool kids
+#
+# (c) 2025 George Lemon | LGPL License
+#          Made by Humans from OpenPeeps
+#          https://github.com/openpeeps/tim
+
+import std/[net, strutils, base64, tables, os, nativesockets, selectors]
+import pkg/checksums/sha1
+
+const
+  GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+type
+  OnMessageProc = proc(client: Socket, data: seq[byte])
+  OnConnectProc = proc(client: Socket)
+  OnCloseProc = proc(client: Socket)
+
+  ClientState = enum
+    Handshaking, Open
+
+  ClientInfo = object
+    sock: Socket
+    buf: string
+    state: ClientState
+
+proc acceptWebSocket(client: Socket, key: string) =
+  let digest = sha1.secureHash(key & GUID)
+  let shaArray = cast[array[0 .. 19, uint8]](digest)
+  let acceptKey = base64.encode(shaArray)
+  client.send("HTTP/1.1 101 Switching Protocols\r\n" &
+              "Upgrade: websocket\r\n" &
+              "Connection: Upgrade\r\n" &
+              "Sec-WebSocket-Accept: " & acceptKey & "\r\n\r\n")
+
+proc getWebSocketKey(header: string): string =
+  for line in header.splitLines():
+    if line.startsWith("Sec-WebSocket-Key:"):
+      return line.split(":")[1].strip()
+  return ""
+
+proc readHttpHeader(client: Socket): string =
+  var header = ""
+  while true:
+    let line = client.recvLine()
+    if line.len == 0: break
+    header.add(line & "\n")
+  return header
+
+proc wsFrameText(data: string): string =
+  result = newStringOfCap(2 + 8 + data.len)
+  result.add char(0x81) # FIN + text frame
+  let L = data.len
+  if L <= 125:
+    result.add char(L)
+  elif L <= 0xFFFF:
+    result.add char(126)
+    result.add char((L shr 8) and 0xFF)
+    result.add char(L and 0xFF)
+  else:
+    result.add char(127)
+    for i in countdown(7, 0):
+      result.add char((uint64(L) shr (i * 8)) and 0xFF)
+  result.add data
+
+proc wsSendText*(client: Socket, data: string) =
+  client.send(wsFrameText(data))
+
+var timWebSocketThread: Thread[tuple[port: Port]]
+var connections: seq[Socket]
+
+proc notifyAllClients*() =
+  ## Notify all connected WebSocket clients
+  for client in connections:
+    if client != nil:
+      client.wsSendText("1")
+
+proc onMessage(client: Socket, data: seq[byte]) =
+  discard
+
+proc onConnect(client: Socket) =
+  connections.add(client)
+
+proc onClose(client: Socket) =
+  while true:
+    let idx = connections.find(client)
+    if idx == -1: break
+    connections.delete(idx)
+
+proc startWebSocket*(port: Port = Port(9000)) =
+  proc run(args: tuple[port: Port]) {.thread.} =
+    {.gcsafe.}:
+      let server = newSocket()
+      server.setSockOpt(OptReusePort, true)
+      server.bindAddr(args.port)
+      server.listen()
+
+      var selector = newSelector[int]()
+      selector.registerHandle(server.getFd, {Event.Read}, 0)
+
+      var clientSockets = initTable[int, ClientInfo]()
+
+      while true:
+        let events = selector.select(-1)
+        for event in events:
+          if SocketHandle(event.fd) == server.getFd:
+            var client: Socket
+            server.accept(client)
+            client.getFd.setBlocking(false)
+            selector.registerHandle(client.getFd, {Event.Read}, 0)
+            clientSockets[client.getFd.int] = ClientInfo(sock: client, buf: "", state: Handshaking)
+          else:
+            var info = clientSockets.getOrDefault(event.fd)
+            if info.sock != nil:
+              var tmp = newString(4096)
+              let n = info.sock.recv(tmp, tmp.len)
+              if n <= 0:
+                onClose(info.sock)
+                selector.unregister(SocketHandle(event.fd))
+                info.sock.close()
+                clientSockets.del(event.fd)
+              else:
+                info.buf.add tmp[0 ..< n]
+                if info.state == Handshaking:
+                  let sep = "\r\n\r\n"
+                  let p = info.buf.find(sep)
+                  if p >= 0:
+                    let headerEnd = p + sep.len
+                    let header = info.buf[0 ..< headerEnd]
+                    let key = getWebSocketKey(header)
+                    if key.len > 0:
+                      acceptWebSocket(info.sock, key)
+                      onConnect(info.sock)
+                      info.state = Open
+                    # drop header from buffer
+                    if headerEnd < info.buf.len:
+                      let remaining = info.buf[headerEnd .. ^1]
+                      if remaining.len > 0:
+                        onMessage(info.sock, cast[seq[byte]](remaining))
+                    info.buf.setLen(0)
+                else:
+                  onMessage(info.sock, cast[seq[byte]](info.buf))
+                  info.buf.setLen(0)
+                clientSockets[event.fd] = info
+  let args = (port: port)
+  createThread(timWebSocketThread, run, args)
